25d3635f1f34e0a3a8ce45fd7f778444639f68fa
Adam Kowalczyk <akowalczyk@euvic.pl>
2019-09-05 22:45:26 +0200

    remove enzyme adapter

--- jest/setup.js
+++ jest/setup.js
@@ -1,9 +1,3 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-
-configure({ adapter: new Adapter() });
-
 jest.mock('react-native-localize', () => ({
   getLocales: () => [
     {
